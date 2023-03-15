53a57f4748821f0b9857792d820e2f5f91caf533
Andhika ramadhani riyadi <74587857+Arondyroz@users.noreply.github.com>
2023-03-15 18:45:09 +0900

    Add files via upload

---
+++ Script/BackgroundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField] private RawImage backgroundImg;
+    [SerializeField] private float xAxis;
+    private RectTransform myRect;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        myRect = GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        Vector2 rectPosX = myRect.localPosition;
+        rectPosX.x--;
+        MoveBackground();
+    }
+
+    private void MoveBackground()
+    {
+        backgroundImg.uvRect = new Rect(backgroundImg.uvRect.position + new Vector2(xAxis, 0) * Time.deltaTime, backgroundImg.uvRect.size);
+    }
+}
---
+++ Script/Turret.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public int turretID;
+    private Transform bulletPoint;
+    private float cooldown;
+    private float cooldownTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPoint = this.gameObject.transform.GetChild(0);
+        cooldown = Random.Range(4, 10);
+        cooldownTimer = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerMovement.instance.isDead == false && PlayerMovement.instance.startGame == true)
+        {
+            FireBullet();
+        }
+    }
+
+    public void FireBullet()
+    {
+        cooldown = Random.Range(3, 10);
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+        else 
+        {
+            cooldownTimer = cooldown;
+            GameObject tempBullet = Instantiate(bulletPrefab, bulletPoint.position, bulletPoint.rotation);
+            Rigidbody2D tempBulletRb = tempBullet.GetComponent<Rigidbody2D>();
+            AudioSource tempBulletClip = tempBullet.GetComponent<AudioSource>();
+
+            tempBulletClip.Play();
+
+            tempBulletRb.AddForce(bulletPoint.up * bulletSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
